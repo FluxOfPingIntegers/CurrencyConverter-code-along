2f300567fa6e14498b741c42da58913a1480f0a7
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-03-15 08:31:31 -0400

    adding button component

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,13 +1,14 @@
 
 
 import React from "react";
-import { Text, View, StyleSheet, StatusBar, Image, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, StatusBar, Image, Dimensions, ScrollView } from 'react-native';
 import { format } from 'date-fns';
 
 import colors from "../constants/colors";
 
 
 import { ConversionInput } from "../components/ConversionInput";
+import { Button } from "../components/Button";
 
 
 export default () => {
@@ -18,41 +19,47 @@ export default () => {
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor={colors.blue}/>
-      <View style={styles.logoContainer}>
-        <Image 
-          source={require('../assets/images/background.png')} 
-          style={styles.logoBackground} 
-          resizeMode="contain"
-        />
-        <Image 
-          source={require('../assets/images/logo.png')} 
-          style={styles.logo} 
-          resizeMode="contain"
-        />
-      </View>
-
-      <Text style={styles.textHeader}>Currency Converter</Text>
-
-      <ConversionInput 
-        text={baseCurrency} 
-        value="123"
-        onButtonPress={() => alert("todo!")} 
-        onChangeText={text => console.log("text", text)}
-        keyboardType="numeric"
-      />
-
-      <ConversionInput 
-        text={quoteCurrency}
-        value="123"
-        onButtonPress={() => alert("todo!")} 
-        editable={false}
-      />
-
-      <Text style={styles.text}>
-        {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}`}
-      </Text>
-
+      <ScrollView>
+        <StatusBar barStyle="light-content" backgroundColor={colors.blue}/>
+          <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Image 
+              source={require('../assets/images/background.png')} 
+              style={styles.logoBackground} 
+              resizeMode="contain"
+            />
+            <Image 
+              source={require('../assets/images/logo.png')} 
+              style={styles.logo} 
+              resizeMode="contain"
+            />
+          </View>
+    
+          <Text style={styles.textHeader}>Currency Converter</Text>
+    
+          <ConversionInput 
+            text={baseCurrency} 
+            value="123"
+            onButtonPress={() => alert("todo!")} 
+            onChangeText={text => console.log("text", text)}
+            keyboardType="numeric"
+          />
+    
+          <ConversionInput 
+            text={quoteCurrency}
+            value="123"
+            onButtonPress={() => alert("todo!")} 
+            editable={false}
+          />
+    
+          <Text style={styles.text}>
+            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}`}
+          </Text>
+    
+          <Button text="Reverse Currencies" onPress={() => alert('todo!')} />
+          <View style={{ height: screen.height }}></View>
+          </View>
+      </ScrollView>
     </View>
   )
 }
@@ -63,7 +70,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.blue,
     flex: 1,
-    justifyContent: 'center',
+  },
+  content: {
+    paddingTop: screen.height * 0.05,
   },
   logoContainer: {
     alignItems: 'center',
